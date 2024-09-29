f2ab355e02f0f550362502d8c843cf0e7d8b9d74
amandayclee <yician.lee@gmail.com>
2024-09-29 16:34:42 -0700

    add suspense in watch page

--- yt-web-client/app/watch/page.tsx
+++ yt-web-client/app/watch/page.tsx
@@ -1,15 +1,25 @@
 'use client';
 
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation'
 
 export default function Watch() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <WatchContent />
+    </Suspense>
+  );
+}
+
+function WatchContent() {
   const videoPrefix = 'https://storage.googleapis.com/ycleee-yt-processed-videos/';
-  const videoSrc = useSearchParams().get('v');
+  const searchParams = useSearchParams();
+  const videoSrc = searchParams.get('v');
 
   return (
     <div>
       <h1>Watch Page</h1>
-      { <video controls src={videoPrefix + videoSrc}/> }
+      {videoSrc && <video controls src={videoPrefix + videoSrc}/>}
     </div>
   );
 }
